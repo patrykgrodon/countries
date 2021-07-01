b76f2fc1974b2a32ed40708fa4210999b3f0266e
kyrtaposs <kyrtapek1@gmail.com>
2021-07-01 11:54:46 +0200

    Added functionality which gives information about country you're in

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
+///////////////////////////////////////////////////
+// Variables
+const locationBtn = document.querySelector(".location__btn");
 const closeModalBtn = document.querySelector(".modal__btn");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const formInput = document.querySelector(".form__input");
 const formBtn = document.querySelector(".form__btn");
-
 const countriesContainer = document.querySelector(".countries-container");
 
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
+/////////////////////////////////////////////
+// Functions
 
 const getCountryData = async function (country) {
   try {
@@ -22,6 +22,8 @@ const getCountryData = async function (country) {
 };
 
 const displayCountry = async function (countryData) {
+  console.log(countryData);
+  console.log(countryData.name, countryData.name.length);
   const countryImg = countryData.flag;
   const countryName = countryData.name;
   const countryCapital = countryData.capital;
@@ -46,10 +48,29 @@ const displayCountry = async function (countryData) {
   countriesContainer.insertAdjacentHTML("afterbegin", html);
 };
 
-closeModalBtn.addEventListener("click", function () {
-  closeModal();
-});
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
+const succes = async function (pos) {
+  const coords = pos.coords;
+  const res = await fetch(
+    `https://geocode.xyz/${coords.latitude},${coords.longitude}?geoit=json`
+  );
+  const { country } = await res.json();
+  const countryData = await getCountryData(country);
+  displayCountry(countryData);
+};
+
+/////////////////////////////////////////////////////
+// Event listeners
+
+// Display country you're in
+locationBtn.addEventListener("click", function () {
+  navigator.geolocation.getCurrentPosition(succes);
+});
+// Display country you're looking for
 formBtn.addEventListener("click", async function (e) {
   e.preventDefault();
   const countryData = await getCountryData(formInput.value);
@@ -57,6 +78,10 @@ formBtn.addEventListener("click", async function (e) {
   formInput.value = "";
 });
 
+// Close Modal
+closeModalBtn.addEventListener("click", function () {
+  closeModal();
+});
 displayMap = function (country) {
   const html = `
     <div class="modal__map">
