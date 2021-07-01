ab92a18675fe219381f49ab8b478232de67de3e8
kyrtaposs <kyrtapek1@gmail.com>
2021-07-01 13:07:10 +0200

    Added display map function

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 ///////////////////////////////////////////////////
 // Variables
+
+// Location
 const locationBtn = document.querySelector(".location__btn");
-const closeModalBtn = document.querySelector(".modal__btn");
+// Modal
 const modal = document.querySelector(".modal");
+const closeModalBtn = document.querySelector(".modal__btn");
 const overlay = document.querySelector(".overlay");
+// Form
 const formInput = document.querySelector(".form__input");
 const formBtn = document.querySelector(".form__btn");
+
+//Countries
 const countriesContainer = document.querySelector(".countries-container");
+const displayMapBtn = document.querySelector("country__map");
+const displayNeighboursBtn = document.querySelector("country__neighbours");
 
 /////////////////////////////////////////////
 // Functions
@@ -22,8 +30,6 @@ const getCountryData = async function (country) {
 };
 
 const displayCountry = async function (countryData) {
-  console.log(countryData);
-  console.log(countryData.name, countryData.name.length);
   const countryImg = countryData.flag;
   const countryName = countryData.name;
   const countryCapital = countryData.capital;
@@ -31,9 +37,11 @@ const displayCountry = async function (countryData) {
   const countryPopulation = countryData.population;
   const countryCurrencies = countryData.currencies[0].name;
 
-  const html = `
-  <div class="country">
-              <img class="country__img" src="${countryImg}" alt="Country image" />
+  const countryEl = document.createElement("div");
+  countryEl.classList.add("country");
+  countryEl.setAttribute("data-alpha2code", `${countryData.alpha2Code}`);
+  countryEl.innerHTML = `
+              <img class="country__img" src="${countryImg}" alt="${countryName} flag" />
             <p class="country__name heading-2">${countryName}</p>
             <div class="country__info-container">
               <p class="country__info">Capital: ${countryCapital}</p>
@@ -43,14 +51,35 @@ const displayCountry = async function (countryData) {
             </div>
             <p class="country__map">Show on map</p>
             <p class="country__neighbours">Neighbours</p>
-          </div>
   `;
-  countriesContainer.insertAdjacentHTML("afterbegin", html);
+  countriesContainer.insertAdjacentElement("afterbegin", countryEl);
+  countryEl.scrollIntoView({ behavior: "smooth" });
 };
 
+displayMap = function (country) {
+  const html = `
+    <div class="modal__map">
+        <iframe
+          width="800"
+          height="600"
+          style="border: 0"
+          loading="lazy"
+          allowfullscreen
+          src="https://www.google.com/maps/embed/v1/place?q=${country}&key=AIzaSyBMqV1gALEitm4NmDfa3ZTvlHe-CgjQlk0"
+        ></iframe>
+      </div>`;
+  closeModalBtn.insertAdjacentHTML("afterend", html);
+};
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
+  if (modal.querySelector(".modal__map"))
+    modal.querySelector(".modal__map").remove();
 };
 
 const succes = async function (pos) {
@@ -78,24 +107,19 @@ formBtn.addEventListener("click", async function (e) {
   formInput.value = "";
 });
 
+// Display map country you want
+countriesContainer.addEventListener("click", function (e) {
+  if (!e.target.classList.contains("country__map")) return;
+  const countryName = e.target
+    .closest(".country")
+    .querySelector(".country__name").innerHTML;
+  openModal();
+  displayMap(countryName);
+});
 // Close Modal
 closeModalBtn.addEventListener("click", function () {
   closeModal();
 });
-displayMap = function (country) {
-  const html = `
-    <div class="modal__map">
-        <iframe
-          width="800"
-          height="600"
-          style="border: 0"
-          loading="lazy"
-          allowfullscreen
-          src="https://www.google.com/maps/embed/v1/place?q=${country}&key=AIzaSyBMqV1gALEitm4NmDfa3ZTvlHe-CgjQlk0"
-        ></iframe>
-      </div>`;
-  btnCloseModal.insertAdjacentHTML("afterend", html);
-};
 
 displayNeighbour = function (countryData) {
   const html = `
